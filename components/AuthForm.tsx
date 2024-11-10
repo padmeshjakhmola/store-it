1abd60b176ec30b9031c9a9215c654967c8ebb43
Padmesh Jakhmola <prankshadow@gmail.com>
2024-11-11 02:03:16 +0530

    3848

---
+++ components/AuthForm.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+type FormType = "sign-in" | "sign-up";
+const AuthForm = ({ type }: { type: FormType }) => {
+  return <div>AuthForm</div>;
+};
+
+export default AuthForm;

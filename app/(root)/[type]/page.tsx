4d05dbdd9c91cc522d61ff42d2787c55a70e53da
Padmesh Jakhmola <prankshadow@gmail.com>
2024-11-18 18:32:06 +0530

    rename

---
+++ app/(root)/[type]/page.tsx
@@ -0,0 +1,45 @@
+import Card from "@/components/Card";
+import Sort from "@/components/Sort";
+import { getFiles } from "@/lib/actions/file.action";
+import { SearchParamProps } from "@/types";
+import { Models } from "node-appwrite";
+import React from "react";
+
+const Page = async ({ params }: SearchParamProps) => {
+  const type = ((await params)?.type as string) || "";
+
+  const files = await getFiles({ types: ["image"] });
+  //   const files = [];
+
+  return (
+    <div className="page-container">
+      <section className="w-full">
+        <h1 className="h1 capitalize">{type}</h1>
+
+        <div className="total-size-section">
+          <p className="body-1">
+            Total:<span className="h5">0 MB</span>
+          </p>
+
+          <div className="sort-container">
+            <p className="body-1 hidden text-light-200 sm:block">Sort By:</p>
+
+            <Sort />
+          </div>
+        </div>
+      </section>
+
+      {files.total > 0 ? (
+        <section className="file-list">
+          {files.documents.map((file: Models.Document) => (
+            <Card key={file.$id} file={file} />
+          ))}
+        </section>
+      ) : (
+        <p className="empty-list">No files uploded</p>
+      )}
+    </div>
+  );
+};
+
+export default Page;

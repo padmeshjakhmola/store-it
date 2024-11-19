2ba3f381251a688e6ad289d952fdcd0b6834404e
Padmesh Jakhmola <prankshadow@gmail.com>
2024-11-19 15:06:59 +0530

    minor fix

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,6 +6,9 @@ import { redirect } from "next/navigation";
 import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 
+export const dynamic = "force-dynamic";
+// production fix as it takes cookies
+
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const currentUser = await getCurrentUser();
 
@@ -14,7 +17,8 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
     <main className="flex h-screen">
       <Sidebar {...currentUser} />
       <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation {...currentUser} /> <Header userId={currentUser.$id} accountId={currentUser.accountId} />
+        <MobileNavigation {...currentUser} />{" "}
+        <Header userId={currentUser.$id} accountId={currentUser.accountId} />
         <div className="main-content">{children}</div>
       </section>
       <Toaster />
